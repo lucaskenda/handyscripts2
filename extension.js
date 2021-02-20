09e3baa3d54e774f060920f613c5b03cfa0f6658
lucaskenda <lucaskenda@gmail.com>
2021-02-20 13:14:29 -0300

    Customize python and bash commands

--- extension.js
+++ extension.js
@@ -13,12 +13,17 @@ const Lang = imports.lang;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 
-const DefaultFolder = GLib.build_filenamev([global.userdatadir, 'extensions/handyscripts2@lucaskenda.com.ar']);
+const DefaultFolder = GLib.build_filenamev([
+  global.userdatadir,
+  "extensions/handyscripts2@lucaskenda.com.ar",
+]);
 
-const SCHEMA = 'org.gnome.shell.extensions.handyscripts2';
-const SCRIPTS_BUTTON_SHOWHIDE = 'scripts-button-show';
-const SCRIPTS_DEFAULT_PATH_ENABLEDISABLE = 'scripts-default-path-enabled';
-const SCRIPTS_FOLDER_PATH = 'scripts-folder-path';
+const SCHEMA = "org.gnome.shell.extensions.handyscripts2";
+const SCRIPTS_BUTTON_SHOWHIDE = "scripts-button-show";
+const SCRIPTS_DEFAULT_PATH_ENABLEDISABLE = "scripts-default-path-enabled";
+const SCRIPTS_FOLDER_PATH = "scripts-folder-path";
+const BASH_COMMMAND = "bash-executable";
+const PYTHON_COMMMAND = "python-executable";
 
 function compare(a, b) {
   if (a.name > b.name) return 1;
@@ -28,207 +33,243 @@ function compare(a, b) {
 
 // Status menu
 const Menu = new Lang.Class({
-    Name: 'Menu.Menu',
-    Extends: PanelMenu.Button,
+  Name: "Menu.Menu",
+  Extends: PanelMenu.Button,
 
-    _init: function () {
+  _init: function () {
+    this.parent(1, "MainPopupMenu", false);
 
-      this.parent(1, 'MainPopupMenu', false);
+    this._settings = Convenience.getSettings(SCHEMA);
 
-			this._settings = Convenience.getSettings(SCHEMA);
+    let box = new St.BoxLayout();
+    let icon = new St.Icon({
+      icon_name: "utilities-terminal-symbolic",
+      style_class: "system-status-icon",
+    });
+    let toplabel = new St.Label({
+      text: "",
+      y_expand: true,
+      y_align: Clutter.ActorAlign.CENTER,
+    });
 
-      let box = new St.BoxLayout();
-      let icon = new St.Icon({ icon_name: 'utilities-terminal-symbolic', style_class: 'system-status-icon'});
-      let toplabel = new St.Label({ text: '',
-        y_expand: true,
-        y_align: Clutter.ActorAlign.CENTER });
+    box.add(icon);
+    box.add(toplabel);
 
-      box.add(icon);
-      box.add(toplabel);
+    this.actor.add_child(box);
+    this.actor.connect(
+      "button_press_event",
+      Lang.bind(this, this._refreshMenu)
+    );
 
-      this.actor.add_child(box);
-      this.actor.connect('button_press_event', Lang.bind(this, this._refreshMenu));
+    this._renderMenu();
+  },
 
-      this._renderMenu();
-    },
+  _executeScript: function (fileName) {
+    let bashCommand = this._settings.get_string(BASH_COMMMAND);
+    let pythonCommand = this._settings.get_string(PYTHON_COMMMAND);
 
-    _executeScript: function(fileName) {
-      try {
-
-        if (fileName.endsWith('.py') || fileName.endsWith('.pyw')) {
-          // Python
-          Util.trySpawnCommandLine('gnome-terminal -- sh -c \"python \'' + fileName + '\';sleep 2\"')
-        } else {
-          // Bash
-          Util.trySpawnCommandLine('gnome-terminal -- sh -c \"bash \'' + fileName + '\';sleep 2\"');
-        }
-
-      } catch(err) {
-        Main.notify('Error: ' + err);
-      }
-    },
-
-    _executeTweakAcces: function() {
-      try {
-  			Util.trySpawnCommandLine('gnome-tweaks');
-      } catch(err) {
-        Main.notify('Error: ' + err);
-      }
-    },
-
-    _openFolder: function() {
-      Util.trySpawnCommandLine('nautilus ' + this.folder);
-    },
-
-    _refreshMenu: function () {
-        if (this.menu.isOpen) {
-            this.menu.removeAll();
-            this._renderMenu();
-        }
-    },
-
-    _renderMenu: function () {
-
-      let file, fileName;
-      let folderArray = [];
-      let filesArray = [];
-      let isEmpty = true;
-
-      // Check if the folder hast a value.
-      if(this._settings.get_string(SCRIPTS_FOLDER_PATH) == '') {
-
-        let popupMenuItem = new PopupMenu.PopupMenuItem('You must configure a folder in GNOME Tweaks!');
-        popupMenuItem.connect('activate', this._executeTweakAcces.bind(this));
-        this.menu.addMenuItem(popupMenuItem);
-
+    try {
+      if (fileName.endsWith(".py") || fileName.endsWith(".pyw")) {
+        // Python
+        Util.trySpawnCommandLine(
+          'gnome-terminal -- sh -c "' +
+            pythonCommand +
+            " '" +
+            fileName +
+            "';sleep 2\""
+        );
       } else {
+        // Bash
+        Util.trySpawnCommandLine(
+          'gnome-terminal -- sh -c "' +
+            bashCommand +
+            " '" +
+            fileName +
+            "';sleep 2\""
+        );
+      }
+    } catch (err) {
+      Main.notify("Error: " + err);
+    }
+  },
 
-        this.folder = this._settings.get_string(SCRIPTS_FOLDER_PATH);
-        this.folder = this.folder.replace(/\/?$/, '/');
+  _executeTweakAcces: function () {
+    try {
+      Util.trySpawnCommandLine("gnome-tweaks");
+    } catch (err) {
+      Main.notify("Error: " + err);
+    }
+  },
 
-        let dir = Gio.file_new_for_path(this.folder);
+  _openFolder: function () {
+    Util.trySpawnCommandLine("nautilus " + this.folder);
+  },
 
-        if(dir.query_exists(null)) {
+  _refreshMenu: function () {
+    if (this.menu.isOpen) {
+      this.menu.removeAll();
+      this._renderMenu();
+    }
+  },
 
-          let files = dir.enumerate_children(Gio.FILE_ATTRIBUTE_STANDARD_NAME, Gio.FileQueryInfoFlags.NONE, null);
+  _renderMenu: function () {
+    let file, fileName;
+    let folderArray = [];
+    let filesArray = [];
+    let isEmpty = true;
 
-          while((file = files.next_file(null))) {
+    // Check if the folder hast a value.
+    if (this._settings.get_string(SCRIPTS_FOLDER_PATH) == "") {
+      let popupMenuItem = new PopupMenu.PopupMenuItem(
+        "You must configure a folder in GNOME Tweaks!"
+      );
+      popupMenuItem.connect("activate", this._executeTweakAcces.bind(this));
+      this.menu.addMenuItem(popupMenuItem);
+    } else {
+      this.folder = this._settings.get_string(SCRIPTS_FOLDER_PATH);
+      this.folder = this.folder.replace(/\/?$/, "/");
 
-            let fileName = file.get_name();
+      let dir = Gio.file_new_for_path(this.folder);
 
-            // Push folders to array.
-            if(file.get_file_type() == Gio.FileType.DIRECTORY) {
+      if (dir.query_exists(null)) {
+        let files = dir.enumerate_children(
+          Gio.FILE_ATTRIBUTE_STANDARD_NAME,
+          Gio.FileQueryInfoFlags.NONE,
+          null
+        );
 
-              let menu = new PopupMenu.PopupSubMenuMenuItem(fileName, true);
-              menu.icon.icon_name = 'folder-symbolic';
+        while ((file = files.next_file(null))) {
+          let fileName = file.get_name();
 
-              folderArray.push({
-                name: fileName,
-                path: this.folder + fileName,
-                submenu: menu
-              });
+          // Push folders to array.
+          if (file.get_file_type() == Gio.FileType.DIRECTORY) {
+            let menu = new PopupMenu.PopupSubMenuMenuItem(fileName, true);
+            menu.icon.icon_name = "folder-symbolic";
 
-              isEmpty = false;
+            folderArray.push({
+              name: fileName,
+              path: this.folder + fileName,
+              submenu: menu,
+            });
 
-            } else {
+            isEmpty = false;
+          } else {
+            // Push first level files to array.
+            if (file.get_file_type() == Gio.FileType.REGULAR) {
+              if (
+                fileName.endsWith(".sh") ||
+                fileName.endsWith(".py") ||
+                fileName.endsWith(".pyw")
+              ) {
+                let popupMenuItem = new PopupMenu.PopupImageMenuItem(
+                  fileName.split(".")[0],
+                  "media-playback-start-symbolic"
+                );
+                popupMenuItem.connect(
+                  "activate",
+                  this._executeScript.bind(this, this.folder + fileName)
+                );
 
-              // Push first level files to array.
-              if(file.get_file_type() == Gio.FileType.REGULAR) {
-
-                if(fileName.endsWith('.sh') || fileName.endsWith('.py') || fileName.endsWith('.pyw')) {
-
-                  let popupMenuItem = new PopupMenu.PopupImageMenuItem(fileName.split('.')[0], 'media-playback-start-symbolic');
-                  popupMenuItem.connect('activate', this._executeScript.bind(this, this.folder + fileName));
-
-                  filesArray.push({
-                    name: fileName,
-                    popupMenuItem: popupMenuItem
-                  });
-
-                  isEmpty = false;
-                }
-
-              }
-            }
-
-          }
-
-          // Sort first level files.
-          filesArray.sort(compare);
-
-          // Sort folders.
-          folderArray.sort(compare);
-
-          // For each folder list files that ends with .sh and add them to the menu array.
-          for(var i=0; i < folderArray.length; i++) {
-
-            dir = Gio.file_new_for_path(folderArray[i].path);
-            files = dir.enumerate_children(Gio.FILE_ATTRIBUTE_STANDARD_NAME, Gio.FileQueryInfoFlags.NONE, null);
-
-            let file;
-            let subfilesArray = [];
-
-            while((file = files.next_file(null))) {
-
-              let fileName = file.get_name();
-
-              if(fileName.endsWith('.sh') || fileName.endsWith('.py') || fileName.endsWith('.pyw')) {
-
-                let popupMenuItem = new PopupMenu.PopupImageMenuItem(fileName.split('.')[0], 'media-playback-start-symbolic');
-                popupMenuItem.connect('activate', this._executeScript.bind(this, folderArray[i].path + '/' + fileName));
-
-                subfilesArray.push({
+                filesArray.push({
                   name: fileName,
-                  popupMenuItem: popupMenuItem
+                  popupMenuItem: popupMenuItem,
                 });
 
+                isEmpty = false;
               }
-
             }
-
-            // Sort second level files and show in menu.
-            subfilesArray.sort(compare);
-
-            for(let j=0; j < subfilesArray.length; j++) {
-              folderArray[i].submenu.menu.addMenuItem(subfilesArray[j].popupMenuItem);
-            }
-
-            this.menu.addMenuItem(folderArray[i].submenu);
-
           }
-
-	  // Add files to menu.
-          for(let i=0; i < filesArray.length; i++) {
-            this.menu.addMenuItem(filesArray[i].popupMenuItem);
-          }
-
-          if(isEmpty) {
-            // If is empty.
-            let Scripts = new PopupMenu.PopupMenuItem('Can\'t find scripts!');
-            this.menu.addMenuItem(Scripts);
-          }
-
-          // Scripts Folder menu.
-          if(this._settings.get_boolean(SCRIPTS_BUTTON_SHOWHIDE) == true) {
-            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-            let Scripts = new PopupMenu.PopupImageMenuItem('Scripts', 'system-run-symbolic');
-            this.menu.addMenuItem(Scripts);
-            Scripts.connect('activate', this._openFolder.bind(this));
-          }
-
-        } else {
-
-          // If folder does not exists.
-          let Scripts = new PopupMenu.PopupMenuItem('Folder path does not exists!');
-          this.menu.addMenuItem(Scripts);
-
         }
 
+        // Sort first level files.
+        filesArray.sort(compare);
+
+        // Sort folders.
+        folderArray.sort(compare);
+
+        // For each folder list files that ends with .sh and add them to the menu array.
+        for (var i = 0; i < folderArray.length; i++) {
+          dir = Gio.file_new_for_path(folderArray[i].path);
+          files = dir.enumerate_children(
+            Gio.FILE_ATTRIBUTE_STANDARD_NAME,
+            Gio.FileQueryInfoFlags.NONE,
+            null
+          );
+
+          let file;
+          let subfilesArray = [];
+
+          while ((file = files.next_file(null))) {
+            let fileName = file.get_name();
+
+            if (
+              fileName.endsWith(".sh") ||
+              fileName.endsWith(".py") ||
+              fileName.endsWith(".pyw")
+            ) {
+              let popupMenuItem = new PopupMenu.PopupImageMenuItem(
+                fileName.split(".")[0],
+                "media-playback-start-symbolic"
+              );
+              popupMenuItem.connect(
+                "activate",
+                this._executeScript.bind(
+                  this,
+                  folderArray[i].path + "/" + fileName
+                )
+              );
+
+              subfilesArray.push({
+                name: fileName,
+                popupMenuItem: popupMenuItem,
+              });
+            }
+          }
+
+          // Sort second level files and show in menu.
+          subfilesArray.sort(compare);
+
+          for (let j = 0; j < subfilesArray.length; j++) {
+            folderArray[i].submenu.menu.addMenuItem(
+              subfilesArray[j].popupMenuItem
+            );
+          }
+
+          this.menu.addMenuItem(folderArray[i].submenu);
+        }
+
+        // Add files to menu.
+        for (let i = 0; i < filesArray.length; i++) {
+          this.menu.addMenuItem(filesArray[i].popupMenuItem);
+        }
+
+        if (isEmpty) {
+          // If is empty.
+          let Scripts = new PopupMenu.PopupMenuItem("Can't find scripts!");
+          this.menu.addMenuItem(Scripts);
+        }
+
+        // Scripts Folder menu.
+        if (this._settings.get_boolean(SCRIPTS_BUTTON_SHOWHIDE) == true) {
+          this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+          let Scripts = new PopupMenu.PopupImageMenuItem(
+            "Scripts",
+            "system-run-symbolic"
+          );
+          this.menu.addMenuItem(Scripts);
+          Scripts.connect("activate", this._openFolder.bind(this));
+        }
+      } else {
+        // If folder does not exists.
+        let Scripts = new PopupMenu.PopupMenuItem(
+          "Folder path does not exists!"
+        );
+        this.menu.addMenuItem(Scripts);
       }
-
-      this.actor.show();
-
     }
+
+    this.actor.show();
+  },
 });
 
 // Menu variable
@@ -236,17 +277,19 @@ let menu;
 let settings;
 
 function init() {
-	log(`Initializing ${Me.metadata.name} version ${Me.metadata.version}`);
-	this._settings = Convenience.getSettings('org.gnome.shell.extensions.handyscripts2');
+  log(`Initializing ${Me.metadata.name} version ${Me.metadata.version}`);
+  this._settings = Convenience.getSettings(
+    "org.gnome.shell.extensions.handyscripts2"
+  );
 }
 
 function enable() {
-	log(`Enabling ${Me.metadata.name} version ${Me.metadata.version}`);
-	menu = new Menu();
-	Main.panel.addToStatusArea('handy-scripts2', menu, 0, 'right');
+  log(`Enabling ${Me.metadata.name} version ${Me.metadata.version}`);
+  menu = new Menu();
+  Main.panel.addToStatusArea("handy-scripts2", menu, 0, "right");
 }
 
 function disable() {
-	log(`Disabling ${Me.metadata.name} version ${Me.metadata.version}`);
-	menu.destroy();
+  log(`Disabling ${Me.metadata.name} version ${Me.metadata.version}`);
+  menu.destroy();
 }
--- prefs.js
+++ prefs.js
@@ -4,135 +4,138 @@ const Lang = imports.lang;
 const Soup = imports.gi.Soup;
 const Gio = imports.gi.Gio;
 
-const Gettext = imports.gettext.domain('handyscripts2');
+const Gettext = imports.gettext.domain("handyscripts2");
 const _ = Gettext.gettext;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 
-const SCHEMA = 'org.gnome.shell.extensions.handyscripts2';
-const SCRIPTS_BUTTON_SHOWHIDE = 'scripts-button-show';
-const SCRIPTS_FOLDER_PATH = 'scripts-folder-path';
+const SCHEMA = "org.gnome.shell.extensions.handyscripts2";
+const SCRIPTS_BUTTON_SHOWHIDE = "scripts-button-show";
+const SCRIPTS_FOLDER_PATH = "scripts-folder-path";
+const BASH_COMMMAND = "bash-executable";
+const PYTHON_COMMMAND = "python-executable";
 
-const ScriptsDir = 'scripts';
+const ScriptsDir = "scripts";
 
 let settings;
 
 function init() {
-	settings = Convenience.getSettings(SCHEMA);
-	Convenience.initTranslations('handyscripts2');
+  settings = Convenience.getSettings(SCHEMA);
+  Convenience.initTranslations("handyscripts2");
 }
 
 function buildPrefsWidget() {
+  let frame = new Gtk.Box({
+    orientation: Gtk.Orientation.VERTICAL,
+    border_width: 10,
+    margin: 20,
+  });
 
-	let frame = new Gtk.Box({
-		orientation: Gtk.Orientation.VERTICAL,
-		border_width: 10, margin: 20});
+  frame.set_spacing(10);
 
-	frame.set_spacing(10);
+  let showScriptsButtonInMenuToggle = _createCheckBox(
+    SCRIPTS_BUTTON_SHOWHIDE,
+    _("Scripts folder link in menu"),
+    _("Enable/Disable default Scripts folder path."),
+    null
+  );
 
-	let showScriptsButtonInMenuToggle = _createCheckBox(
-		SCRIPTS_BUTTON_SHOWHIDE,
-		_('Scripts folder link in menu'),
-		_('Enable/Disable default Scripts folder path.'),
-		null
-	);
+  let scriptFolderEntry = _createEntry(
+    SCRIPTS_FOLDER_PATH,
+    _("Scripts folder path"),
+    _("Path where are located the scripts."),
+    "",
+    true
+  );
 
-	let scriptFolderEntry = _createOnlyEntry(
-		SCRIPTS_FOLDER_PATH,
-		_('Scripts folder path'),
-		_('Path where are located the scripts.'),
-		'',
-		true
-	);
+  let bashCommand = _createEntry(
+    BASH_COMMMAND,
+    _("Bash command"),
+    _("Command to execute bash scripts."),
+    "",
+    true
+  );
 
-	frame.add(showScriptsButtonInMenuToggle);
-	frame.add(scriptFolderEntry);
-	frame.show_all();
+  let pythonCommand = _createEntry(
+    PYTHON_COMMMAND,
+    _("Python command"),
+    _("Command to execute Python scripts."),
+    "",
+    true
+  );
 
-	return frame;
+  frame.add(showScriptsButtonInMenuToggle);
+  frame.add(scriptFolderEntry);
+  frame.add(bashCommand);
+  frame.add(pythonCommand);
+  frame.show_all();
+
+  return frame;
 }
 
 function _createCheckBox(key, text, tooltip, changeFunction) {
+  let box = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+  let label = new Gtk.Label({ label: text, xalign: 0, tooltip_text: tooltip });
+  let widget = new Gtk.Switch({ active: settings.get_boolean(key) });
 
-	let box = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
-	let label = new Gtk.Label({ label: text, xalign: 0, tooltip_text:tooltip });
-	let widget = new Gtk.Switch({ active: settings.get_boolean(key) });
+  widget.connect("notify::active", function (switch_widget) {
+    settings.set_boolean(key, switch_widget.active);
+    changeFunction(switch_widget.active);
+  });
 
-	widget.connect('notify::active', function(switch_widget) {
-		settings.set_boolean(key, switch_widget.active);
-		changeFunction(switch_widget.active);
-	});
+  box.pack_start(label, true, true, 0);
+  box.add(widget);
 
-	box.pack_start(label, true, true, 0);
-	box.add(widget);
-
-	return box;
+  return box;
 }
 
 function _createLabel(text, tooltip) {
-	let label = new Gtk.Label({ label: text, xalign: 0, tooltip_text:tooltip });
-	return label;
+  let label = new Gtk.Label({ label: text, xalign: 0, tooltip_text: tooltip });
+  return label;
 }
 
 function _createComboBox(key, text, tooltip, values) {
+  let box = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+  let label = new Gtk.Label({ label: text, xalign: 0, tooltip_text: tooltip });
+  let widget = new Gtk.ComboBoxText();
 
-	let box = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
-	let label = new Gtk.Label({ label: text, xalign: 0, tooltip_text:tooltip });
-	let widget = new Gtk.ComboBoxText();
+  Object.keys(values).forEach(function (i) {
+    widget.append(i, values[i]);
+  });
 
-	Object.keys(values).forEach(function(i) {
-	   widget.append(i, values[i]);
-	});
+  widget.set_active_id(settings.get_string(key));
+  widget.connect("changed", function (combo_widget) {
+    settings.set_string(key, combo_widget.get_active_id());
+  });
 
-	widget.set_active_id(settings.get_string(key));
-	widget.connect('changed', function(combo_widget) {
-		settings.set_string(key, combo_widget.get_active_id());
-	});
+  box.pack_start(label, true, true, 0);
+  box.add(widget);
 
-	box.pack_start(label, true, true, 0);
-	box.add(widget);
-
-	return box;
+  return box;
 }
 
 function _createButton(text, tooltip) {
-	let widget = new Gtk.Button({ label: text });
-	return widget;
+  let widget = new Gtk.Button({ label: text });
+  return widget;
 }
 
 function _createEntry(key, text, tooltip, placeholder, setVisibility) {
+  let box = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+  let label = new Gtk.Label({ label: text, xalign: 0, tooltip_text: tooltip });
+  let widget = new Gtk.Entry();
 
-	let box = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
-	let label = new Gtk.Label({ label: text, xalign: 0, tooltip_text:tooltip });
-	let widget = new Gtk.Entry();
+  widget.set_placeholder_text(placeholder);
+  widget.set_visibility(setVisibility);
+  widget.text = settings.get_string(key);
+  widget.connect("changed", function () {
+    settings.set_string(key, widget.get_text());
+  });
+  widget.set_width_chars(40);
 
-	widget.set_placeholder_text(placeholder);
-	widget.set_visibility(setVisibility);
-	widget.text = settings.get_string(key);
-	widget.connect('changed', function() {
-		settings.set_string(key, widget.get_text());
-	});
+  box.pack_start(label, true, true, 0);
+  box.add(widget);
 
-	box.pack_start(label, true, true, 0);
-	box.add(widget);
-
-	return box;
-}
-
-function _createOnlyEntry(key, text, tooltip, placeholder, setVisibility) {
-
-	let widget = new Gtk.Entry();
-
-	widget.text = settings.get_string(key);
-
-	widget.connect('changed', function() {
-		settings.set_string(key, widget.get_text());
-	});
-
-	widget.set_placeholder_text(placeholder);
-	widget.set_visibility(setVisibility);
-
-	return widget;
+  return box;
 }
